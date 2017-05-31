4f0ea9657b9e0949ca85874e31c805ee08fe6ecc
Iva Petkova <iva.g.petkova@abv.bg>
2017-05-31 17:54:24 +0300

    Removed views, styles and js files in src folder

---
+++ src/js/App.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Config = require('./Config');
+const template = require('views/pages/index.hbs');
+const data = require('./weather data/ForecastWeather');
+
+class App {
+
+	constructor(){
+		this.update();
+	}
+
+// Public methods -------------------------------------------------------
+	update(value){
+		this._render(value);
+	}
+
+// Private methods ------------------------------------------------------
+	_render(value){
+
+let html = template(data);
+let container = document.querySelector('.app-component');
+container.innerHTML = html;
+
+	}
+}
+
+module.exports = App;
