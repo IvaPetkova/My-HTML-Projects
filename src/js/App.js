69a19e655905dcfbb49ae3e244cb5a943979833c
Iva Petkova <iva.g.petkova@gmail.com>
2017-07-19 00:40:02 +0300

    Modified App.js

--- src/js/App.js
+++ src/js/App.js
@@ -1,28 +1,30 @@
 'use strict';
 
-// const Config = require('./Config');
-// const template = require('views/pages/index.hbs');
-// const data = require('./data/ForecastWeather');
+const template = require('../views/pages/App.hbs');
+const Search = require('./pages/search');
 
-// class AppComponent {
+class App {
 
-// 	constructor() {
-// 		this.update();
-// 	}
+	constructor() {
+		this.update();
+	}
 
-// 	// Public methods -------------------------------------------------------
-// 	update(value) {
-// 		this._render(value);
-// 	}
+	// Public methods -------------------------------------------------------
+	update(value) {
+		this._render(value);
+	}
 
-// 	// Private methods ------------------------------------------------------
-// 	_render(value) {
+	// Private methods ------------------------------------------------------
+	_render(value) {
+		let html = template();
+		let container = document.querySelector('.app-container');
+			container.innerHTML = html;
 
-// 		let html = template(data);
-// 		let container = document.querySelector('.app-component');
-// 		container.innerHTML = html;
+		let search = new Search();
+		container.querySelector('.app-component').appendChild(search.container);
+		// console.error(search);
 
-// 	}
-// }
+	}
+}
 
-// module.exports = AppCoomponent;
+module.exports = App;
