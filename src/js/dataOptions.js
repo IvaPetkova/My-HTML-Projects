6eef7420e8c6ea31936dd4409a60e6fc0161f966
Iva Petkova <iva.g.petkova@gmail.com>
2017-07-19 00:37:09 +0300

    Changed and deleted

--- src/js/dataOptions.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-
-	wrapper = document.querySelector('.app-component'),
-		htmlTemplate = document.querySelector('.weather-template').innerText,
-		template = Handlebars.compile(htmlTemplate);
-
-	wrapper.innerHTML = template();
-};
