7d13e40b9a343188477efa8b3e7fefeb3c5f0b84
Iva Petkova <iva.g.petkova@gmail.com>
2017-07-22 18:08:27 +0300

    Deleted build folder

--- build/js/dataOptions.js
+++
@@ -1,9 +0,0 @@
-// window.onload = function () {
-
-// 	wrapper = document.querySelector('.app-container'),
-// 		htmlTemplate = document.querySelector('.weather-template').innerText,
-// 		template = Handlebars.compile(htmlTemplate);
-
-// 	wrapper.innerHTML = template();
-// };
-"use strict";
